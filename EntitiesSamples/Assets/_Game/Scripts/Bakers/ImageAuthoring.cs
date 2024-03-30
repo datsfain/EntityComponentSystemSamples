2ee56d3fde30e882167cc077f77b3570512e9871
IrakliTurabelidze <datsfain@gmail.com>
2024-03-30 21:12:17 +0400

    added particles and changed urp settings

---
+++ EntitiesSamples/Assets/_Game/Scripts/Bakers/ImageAuthoring.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class ImageAuthoring : MonoBehaviour
+{
+    public class Baker : Baker<ImageAuthoring>
+    {
+        public override void Bake(ImageAuthoring authoring)
+        {
+
+        }
+    }
+}
